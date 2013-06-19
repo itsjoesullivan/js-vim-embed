5c9d766f63d00ab0ec6b70e95a58ef568cb00475
itsjoesullivan <itsjoesullivan@gmail.com>
2013-06-18 20:39:10 -0400

    Better wrapper

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var Vim = require('js-vim'),
 	View = require('./lib/view'),
 	Keys = require('./lib/keys');
 
+require('./lib/style');
+
 /* set up */
 var init = function(obj) {
 
@@ -10,11 +12,22 @@ var init = function(obj) {
 
 	//Hmm.
 	vim.edit = function(obj) {
+		if(!obj || typeof obj !== 'object' || !('el' in obj)) throw "vim.edit required { el: <HTMLElement> }";
+		var text = obj.el.innerHTML;
+		obj.el.innerHTML = '';
 		vim.view = new View({
 			el: obj.el,
 			vim: vim
 		});
+		if(text.length) {
+			vim.curDoc.text(text);
+			vim.exec('G')
+			vim.exec('$')
+		}
+	
 	};
+
+	
 
 	//Set up keys
 	keys = new Keys();
