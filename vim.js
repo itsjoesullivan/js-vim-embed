6c599da78894de587f6ee77bca6b02500b335a98
itsjoesullivan <itsjoesullivan@gmail.com>
2013-07-16 20:06:37 -0400

    update

--- vim.js
+++ vim.js
@@ -5378,7 +5378,81 @@ var mark = module.exports = function(str, mk) {
 	return str;
 };
 
-},{"underscore":18}],12:[function(require,module,exports){
+},{"underscore":18}],24:[function(require,module,exports){
+var Event = require('./Event');
+
+var Cursor = function(obj) {
+
+	this._line = 0;
+	this._char = 0;
+
+	if (typeof obj === 'object') {
+		if ('line' in obj) this._line = obj.line;
+		if ('char' in obj) this._char = obj.char;
+	}
+
+	//Whether the cursor has changed since last set
+	this.moved = false;
+
+	this.on('change', function() {
+		this.moved = true;
+	});
+};
+
+
+Cursor.prototype = new Event();
+
+Cursor.prototype.line = function(num) {
+	if (typeof num === 'number' && this._line !== num) {
+		this._line = num;
+		this.trigger('change:line', num);
+		this.trigger('change');
+	}
+
+	if ('doc' in this) {
+		if (this.doc._lines.length && this.doc._lines.length <= this._line) {
+			this.line(this.doc._lines.length - 1);
+		}
+	}
+
+	return this._line;
+};
+
+Cursor.prototype.col = function(num) {
+	if (typeof num === 'number' && this._char !== num) {
+		this._char = num;
+		this.trigger('change:char', num);
+		this.trigger('change');
+	}
+
+	//Just say we are on the last character if it's not available.
+	if ('doc' in this) {
+		if (this.doc.line().length < this._char) {
+			return this.doc.line().length;
+		}
+	}
+	return this._char;
+};
+
+Cursor.prototype.char = function() {
+	return this.col.apply(this, arguments);
+};
+
+Cursor.prototype.position = function(pos) {
+	if (pos) {
+		this.line(pos.line);
+		this.char(pos.char);
+	}
+	return {
+		line: this.line(),
+		char: this.char(),
+		col: this.col()
+	}
+};
+
+module.exports = Cursor;
+
+},{"./Event":21}],12:[function(require,module,exports){
 (function(){var Cursor = require('./Cursor');
 var _ = require('underscore');
 
@@ -5859,81 +5933,7 @@ Doc.prototype.exec = function() {
 module.exports = Doc;
 
 })()
-},{"./Cursor":24,"./mark":11,"./Event":21,"./Undo":22,"underscore":18}],24:[function(require,module,exports){
-var Event = require('./Event');
-
-var Cursor = function(obj) {
-
-	this._line = 0;
-	this._char = 0;
-
-	if (typeof obj === 'object') {
-		if ('line' in obj) this._line = obj.line;
-		if ('char' in obj) this._char = obj.char;
-	}
-
-	//Whether the cursor has changed since last set
-	this.moved = false;
-
-	this.on('change', function() {
-		this.moved = true;
-	});
-};
-
-
-Cursor.prototype = new Event();
-
-Cursor.prototype.line = function(num) {
-	if (typeof num === 'number' && this._line !== num) {
-		this._line = num;
-		this.trigger('change:line', num);
-		this.trigger('change');
-	}
-
-	if ('doc' in this) {
-		if (this.doc._lines.length && this.doc._lines.length <= this._line) {
-			this.line(this.doc._lines.length - 1);
-		}
-	}
-
-	return this._line;
-};
-
-Cursor.prototype.col = function(num) {
-	if (typeof num === 'number' && this._char !== num) {
-		this._char = num;
-		this.trigger('change:char', num);
-		this.trigger('change');
-	}
-
-	//Just say we are on the last character if it's not available.
-	if ('doc' in this) {
-		if (this.doc.line().length < this._char) {
-			return this.doc.line().length;
-		}
-	}
-	return this._char;
-};
-
-Cursor.prototype.char = function() {
-	return this.col.apply(this, arguments);
-};
-
-Cursor.prototype.position = function(pos) {
-	if (pos) {
-		this.line(pos.line);
-		this.char(pos.char);
-	}
-	return {
-		line: this.line(),
-		char: this.char(),
-		col: this.col()
-	}
-};
-
-module.exports = Cursor;
-
-},{"./Event":21}],13:[function(require,module,exports){
+},{"./Cursor":24,"./mark":11,"./Event":21,"./Undo":22,"underscore":18}],13:[function(require,module,exports){
 var _ = require('underscore'),
 	Set = require('get-set');
 
@@ -6362,130 +6362,7 @@ module.exports.prototype.trigger = function(name, arg1, arg2 /** ... */) {
   return this;
 
 };
-},{}],14:[function(require,module,exports){
-var _ = require('underscore');
-var mark = require('../mark');
-
-module.exports = {
-
-	/* Any time you receive multiple keys in one go */
-	'/^((?!\b|esc)[\\w\\W][\\w\\W]+)$/': function(keys, vim) {
-		for (var i = 0; i < keys.length; i++) {
-			this.exec(keys.substring(i, i + 1));
-		}
-	},
-
-	'/^((?!\b|esc|}).)$/': function(keys, vim, match) {
-		this.currentInsertedText += keys
-		vim.insert(keys);
-	},
-
-	'/^}$/': function() {
-		if (this.rc.smartindent && this.curDoc._lines[this.curDoc.cursor.line()].match(/^\s*$/)) {
-			var ct = this.rc.tabstop;
-			this.exec('esc');
-			while (ct--) {
-				this.exec('X');
-			}
-			this.exec('i');
-		}
-		this.insert('}');
-
-	},
-
-	'/^\n$/': function(keys) {
-		this.currentInsertedText += keys;
-		if (!this.rc.smartindent || this.curDoc._lines[this.curDoc.cursor.line()].length === 0) {
-			this.insert('\n');
-		} else {
-			var thisLine = this.curDoc._lines[this.curDoc.cursor.line()];
-			var indent = thisLine.match(/{\s*(?:\/\/.*|\/\*.*\*\/\s*)?$/);
-			curChar = this.curDoc.cursor.char();
-			var currentText = this.currentInsertedText;
-			this.exec('esc');
-			this.exec('^');
-
-			//Assume you're indenting to the first non-blank char
-			var ct = this.curDoc.cursor.char();
-			//Unless it's all blank, then to zero.
-			if (thisLine.match(/^\s*$/)) {
-				ct = 0;
-			}
-
-			this.curDoc.cursor.char(curChar);
-			this.insert('\n');
-
-			if (indent) {
-				ct += this.rc.tabstop;
-			}
-			this.exec('i');
-			this.currentInsertedText = currentText;
-			while (ct-- > 0) {
-				this.exec(' ');
-			}
-		}
-	},
-
-	'/^(\b)$/': function(keys, vim) {
-		if (this.currentInsertedText.length) this.currentInsertedText = this.currentInsertedText.substring(0, this.currentInsertedText.length - 1);
-		var atZero = !vim.curDoc.cursor.char()
-		vim.exec('esc');
-
-		//Only backspace if there's somewhere to go.
-		if (atZero & !vim.curDoc.cursor.line()) return;
-
-		//Do a join if at the beginning (i.e., deleting a carriage return)		
-		if (atZero && vim.curDoc.cursor.line()) {
-			vim.exec('k');
-			vim.exec('J');
-			vim.exec('x');
-		} else {
-			//Otherwise just erase the character. This works because "esc" from insert decrements the cursor.
-			vim.exec('x');
-		}
-		vim.exec('i');
-	},
-	'/^esc/': function(keys, vim) {
-		//Handle text
-
-		vim.mode('command');
-		vim.exec('h');
-
-		if (this.submode === 'block' & !this.currentInsertedText.match(/\n/)) {
-			this.submode = '';
-			var lastText = this.currentInsertedText;
-			//For each line that was a part of that block selection
-			var meaningfulSelection = this.lastSelection.slice(1);
-			_(meaningfulSelection).each(function(range) {
-				//Move to the beginning of the selection on that line
-				if (this.submodeVerb === 'I') {
-					this.curDoc.cursor.position(range[0]);
-					this.exec('i');
-				} else if (this.submodeVerb === 'A') {
-					var newPos = range[1];
-					newPos.char--;
-					newPos.col--;
-					this.curDoc.cursor.position(range[1]);
-					this.exec('a');
-				}
-				this.exec(lastText);
-				this.exec('esc');
-			}, this);
-			this.curDoc.cursor.position(this.lastSelection[0][0]);
-
-		}
-
-		this.register('.', this.currentInsertedText);
-		this.currentInsertedText = this.currentInsertedText.substring(0, 0);
-
-
-
-	},
-
-
-};
-
-},{"../mark":11,"underscore":18}],15:[function(require,module,exports){
+},{}],15:[function(require,module,exports){
 var _ = require('underscore');
 
 module.exports = {
@@ -7296,7 +7173,130 @@ module.exports = {
 
 }
 
-},{"underscore":18}],16:[function(require,module,exports){
+},{"underscore":18}],14:[function(require,module,exports){
+var _ = require('underscore');
+var mark = require('../mark');
+
+module.exports = {
+
+	/* Any time you receive multiple keys in one go */
+	'/^((?!\b|esc)[\\w\\W][\\w\\W]+)$/': function(keys, vim) {
+		for (var i = 0; i < keys.length; i++) {
+			this.exec(keys.substring(i, i + 1));
+		}
+	},
+
+	'/^((?!\b|esc|}).)$/': function(keys, vim, match) {
+		this.currentInsertedText += keys
+		vim.insert(keys);
+	},
+
+	'/^}$/': function() {
+		if (this.rc.smartindent && this.curDoc._lines[this.curDoc.cursor.line()].match(/^\s*$/)) {
+			var ct = this.rc.tabstop;
+			this.exec('esc');
+			while (ct--) {
+				this.exec('X');
+			}
+			this.exec('i');
+		}
+		this.insert('}');
+
+	},
+
+	'/^\n$/': function(keys) {
+		this.currentInsertedText += keys;
+		if (!this.rc.smartindent || this.curDoc._lines[this.curDoc.cursor.line()].length === 0) {
+			this.insert('\n');
+		} else {
+			var thisLine = this.curDoc._lines[this.curDoc.cursor.line()];
+			var indent = thisLine.match(/{\s*(?:\/\/.*|\/\*.*\*\/\s*)?$/);
+			curChar = this.curDoc.cursor.char();
+			var currentText = this.currentInsertedText;
+			this.exec('esc');
+			this.exec('^');
+
+			//Assume you're indenting to the first non-blank char
+			var ct = this.curDoc.cursor.char();
+			//Unless it's all blank, then to zero.
+			if (thisLine.match(/^\s*$/)) {
+				ct = 0;
+			}
+
+			this.curDoc.cursor.char(curChar);
+			this.insert('\n');
+
+			if (indent) {
+				ct += this.rc.tabstop;
+			}
+			this.exec('i');
+			this.currentInsertedText = currentText;
+			while (ct-- > 0) {
+				this.exec(' ');
+			}
+		}
+	},
+
+	'/^(\b)$/': function(keys, vim) {
+		if (this.currentInsertedText.length) this.currentInsertedText = this.currentInsertedText.substring(0, this.currentInsertedText.length - 1);
+		var atZero = !vim.curDoc.cursor.char()
+		vim.exec('esc');
+
+		//Only backspace if there's somewhere to go.
+		if (atZero & !vim.curDoc.cursor.line()) return;
+
+		//Do a join if at the beginning (i.e., deleting a carriage return)		
+		if (atZero && vim.curDoc.cursor.line()) {
+			vim.exec('k');
+			vim.exec('J');
+			vim.exec('x');
+		} else {
+			//Otherwise just erase the character. This works because "esc" from insert decrements the cursor.
+			vim.exec('x');
+		}
+		vim.exec('i');
+	},
+	'/^esc/': function(keys, vim) {
+		//Handle text
+
+		vim.mode('command');
+		vim.exec('h');
+
+		if (this.submode === 'block' & !this.currentInsertedText.match(/\n/)) {
+			this.submode = '';
+			var lastText = this.currentInsertedText;
+			//For each line that was a part of that block selection
+			var meaningfulSelection = this.lastSelection.slice(1);
+			_(meaningfulSelection).each(function(range) {
+				//Move to the beginning of the selection on that line
+				if (this.submodeVerb === 'I') {
+					this.curDoc.cursor.position(range[0]);
+					this.exec('i');
+				} else if (this.submodeVerb === 'A') {
+					var newPos = range[1];
+					newPos.char--;
+					newPos.col--;
+					this.curDoc.cursor.position(range[1]);
+					this.exec('a');
+				}
+				this.exec(lastText);
+				this.exec('esc');
+			}, this);
+			this.curDoc.cursor.position(this.lastSelection[0][0]);
+
+		}
+
+		this.register('.', this.currentInsertedText);
+		this.currentInsertedText = this.currentInsertedText.substring(0, 0);
+
+
+
+	},
+
+
+};
+
+},{"../mark":11,"underscore":18}],16:[function(require,module,exports){
 (function(){var _ = require('underscore');
 var mark = require('../mark');
 
