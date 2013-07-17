6cdca75af3e2ecc94b38b2d9bddcb3ff3820125f
itsjoesullivan <itsjoesullivan@gmail.com>
2013-07-16 22:12:05 -0400

    js-vim update

--- vim.js
+++ vim.js
@@ -6303,7 +6303,7 @@ View.prototype.notify = function(text) {
 	this.trigger('change:status');
 }
 
-},{"underscore":18,"get-set":17,"mauve":25}],23:[function(require,module,exports){
+},{"get-set":17,"underscore":18,"mauve":25}],23:[function(require,module,exports){
 module.exports = function() {};
 
 /** 
@@ -6362,7 +6362,130 @@ module.exports.prototype.trigger = function(name, arg1, arg2 /** ... */) {
   return this;
 
 };
-},{}],15:[function(require,module,exports){
+},{}],14:[function(require,module,exports){
+var _ = require('underscore');
+var mark = require('../mark');
+
+module.exports = {
+
+	/* Any time you receive multiple keys in one go */
+	'/^((?!\b|esc)[\\w\\W][\\w\\W]+)$/': function(keys, vim) {
+		for (var i = 0; i < keys.length; i++) {
+			this.exec(keys.substring(i, i + 1));
+		}
+	},
+
+	'/^((?!\b|esc|}).)$/': function(keys, vim, match) {
+		this.currentInsertedText += keys
+		vim.insert(keys);
+	},
+
+	'/^}$/': function() {
+		if (this.rc.smartindent && this.curDoc._lines[this.curDoc.cursor.line()].match(/^\s*$/)) {
+			var ct = this.rc.tabstop;
+			this.exec('esc');
+			while (ct--) {
+				this.exec('X');
+			}
+			this.exec('i');
+		}
+		this.insert('}');
+
+	},
+
+	'/^\n$/': function(keys) {
+		this.currentInsertedText += keys;
+		if (!this.rc.smartindent || this.curDoc._lines[this.curDoc.cursor.line()].length === 0) {
+			this.insert('\n');
+		} else {
+			var thisLine = this.curDoc._lines[this.curDoc.cursor.line()];
+			var indent = thisLine.match(/{\s*(?:\/\/.*|\/\*.*\*\/\s*)?$/);
+			curChar = this.curDoc.cursor.char();
+			var currentText = this.currentInsertedText;
+			this.exec('esc');
+			this.exec('^');
+
+			//Assume you're indenting to the first non-blank char
+			var ct = this.curDoc.cursor.char();
+			//Unless it's all blank, then to zero.
+			if (thisLine.match(/^\s*$/)) {
+				ct = 0;
+			}
+
+			this.curDoc.cursor.char(curChar);
+			this.insert('\n');
+
+			if (indent) {
+				ct += this.rc.tabstop;
+			}
+			this.exec('i');
+			this.currentInsertedText = currentText;
+			while (ct-- > 0) {
+				this.exec(' ');
+			}
+		}
+	},
+
+	'/^(\b)$/': function(keys, vim) {
+		if (this.currentInsertedText.length) this.currentInsertedText = this.currentInsertedText.substring(0, this.currentInsertedText.length - 1);
+		var atZero = !vim.curDoc.cursor.char()
+		vim.exec('esc');
+
+		//Only backspace if there's somewhere to go.
+		if (atZero & !vim.curDoc.cursor.line()) return;
+
+		//Do a join if at the beginning (i.e., deleting a carriage return)		
+		if (atZero && vim.curDoc.cursor.line()) {
+			vim.exec('k');
+			vim.exec('J');
+			vim.exec('x');
+		} else {
+			//Otherwise just erase the character. This works because "esc" from insert decrements the cursor.
+			vim.exec('x');
+		}
+		vim.exec('i');
+	},
+	'/^esc/': function(keys, vim) {
+		//Handle text
+
+		vim.mode('command');
+		vim.exec('h');
+
+		if (this.submode === 'block' & !this.currentInsertedText.match(/\n/)) {
+			this.submode = '';
+			var lastText = this.currentInsertedText;
+			//For each line that was a part of that block selection
+			var meaningfulSelection = this.lastSelection.slice(1);
+			_(meaningfulSelection).each(function(range) {
+				//Move to the beginning of the selection on that line
+				if (this.submodeVerb === 'I') {
+					this.curDoc.cursor.position(range[0]);
+					this.exec('i');
+				} else if (this.submodeVerb === 'A') {
+					var newPos = range[1];
+					newPos.char--;
+					newPos.col--;
+					this.curDoc.cursor.position(range[1]);
+					this.exec('a');
+				}
+				this.exec(lastText);
+				this.exec('esc');
+			}, this);
+			this.curDoc.cursor.position(this.lastSelection[0][0]);
+
+		}
+
+		this.register('.', this.currentInsertedText);
+		this.currentInsertedText = this.currentInsertedText.substring(0, 0);
+
+
+
+	},
+
+
+};
+
+},{"../mark":11,"underscore":18}],15:[function(require,module,exports){
 var _ = require('underscore');
 
 module.exports = {
@@ -7173,130 +7296,7 @@ module.exports = {
 
 }
 
-},{"underscore":18}],14:[function(require,module,exports){
-var _ = require('underscore');
-var mark = require('../mark');
-
-module.exports = {
-
-	/* Any time you receive multiple keys in one go */
-	'/^((?!\b|esc)[\\w\\W][\\w\\W]+)$/': function(keys, vim) {
-		for (var i = 0; i < keys.length; i++) {
-			this.exec(keys.substring(i, i + 1));
-		}
-	},
-
-	'/^((?!\b|esc|}).)$/': function(keys, vim, match) {
-		this.currentInsertedText += keys
-		vim.insert(keys);
-	},
-
-	'/^}$/': function() {
-		if (this.rc.smartindent && this.curDoc._lines[this.curDoc.cursor.line()].match(/^\s*$/)) {
-			var ct = this.rc.tabstop;
-			this.exec('esc');
-			while (ct--) {
-				this.exec('X');
-			}
-			this.exec('i');
-		}
-		this.insert('}');
-
-	},
-
-	'/^\n$/': function(keys) {
-		this.currentInsertedText += keys;
-		if (!this.rc.smartindent || this.curDoc._lines[this.curDoc.cursor.line()].length === 0) {
-			this.insert('\n');
-		} else {
-			var thisLine = this.curDoc._lines[this.curDoc.cursor.line()];
-			var indent = thisLine.match(/{\s*(?:\/\/.*|\/\*.*\*\/\s*)?$/);
-			curChar = this.curDoc.cursor.char();
-			var currentText = this.currentInsertedText;
-			this.exec('esc');
-			this.exec('^');
-
-			//Assume you're indenting to the first non-blank char
-			var ct = this.curDoc.cursor.char();
-			//Unless it's all blank, then to zero.
-			if (thisLine.match(/^\s*$/)) {
-				ct = 0;
-			}
-
-			this.curDoc.cursor.char(curChar);
-			this.insert('\n');
-
-			if (indent) {
-				ct += this.rc.tabstop;
-			}
-			this.exec('i');
-			this.currentInsertedText = currentText;
-			while (ct-- > 0) {
-				this.exec(' ');
-			}
-		}
-	},
-
-	'/^(\b)$/': function(keys, vim) {
-		if (this.currentInsertedText.length) this.currentInsertedText = this.currentInsertedText.substring(0, this.currentInsertedText.length - 1);
-		var atZero = !vim.curDoc.cursor.char()
-		vim.exec('esc');
-
-		//Only backspace if there's somewhere to go.
-		if (atZero & !vim.curDoc.cursor.line()) return;
-
-		//Do a join if at the beginning (i.e., deleting a carriage return)		
-		if (atZero && vim.curDoc.cursor.line()) {
-			vim.exec('k');
-			vim.exec('J');
-			vim.exec('x');
-		} else {
-			//Otherwise just erase the character. This works because "esc" from insert decrements the cursor.
-			vim.exec('x');
-		}
-		vim.exec('i');
-	},
-	'/^esc/': function(keys, vim) {
-		//Handle text
-
-		vim.mode('command');
-		vim.exec('h');
-
-		if (this.submode === 'block' & !this.currentInsertedText.match(/\n/)) {
-			this.submode = '';
-			var lastText = this.currentInsertedText;
-			//For each line that was a part of that block selection
-			var meaningfulSelection = this.lastSelection.slice(1);
-			_(meaningfulSelection).each(function(range) {
-				//Move to the beginning of the selection on that line
-				if (this.submodeVerb === 'I') {
-					this.curDoc.cursor.position(range[0]);
-					this.exec('i');
-				} else if (this.submodeVerb === 'A') {
-					var newPos = range[1];
-					newPos.char--;
-					newPos.col--;
-					this.curDoc.cursor.position(range[1]);
-					this.exec('a');
-				}
-				this.exec(lastText);
-				this.exec('esc');
-			}, this);
-			this.curDoc.cursor.position(this.lastSelection[0][0]);
-
-		}
-
-		this.register('.', this.currentInsertedText);
-		this.currentInsertedText = this.currentInsertedText.substring(0, 0);
-
-
-
-	},
-
-
-};
-
-},{"../mark":11,"underscore":18}],16:[function(require,module,exports){
+},{"underscore":18}],16:[function(require,module,exports){
 (function(){var _ = require('underscore');
 var mark = require('../mark');
 
@@ -7874,7 +7874,7 @@ function hex2Address(hex) {
 
 module.exports = mauve;
 
-},{"rgb":26,"x256":27}],26:[function(require,module,exports){
+},{"x256":26,"rgb":27}],27:[function(require,module,exports){
 /*
 color
 */"use strict"
@@ -8012,7 +8012,7 @@ color.matches = function(string){
 
 module.exports = color
 
-},{}],27:[function(require,module,exports){
+},{}],26:[function(require,module,exports){
 // colors scraped from
 // http://www.calmar.ws/vim/256-xterm-24bit-rgb-color-chart.html
 // %s/ *\d\+ \+#\([^ ]\+\)/\1\r/g
